b1fd3a7d48855744e49628578abf88e606654296
Antriksh Singh <a0s00vf@homeoffice.wal-mart.com>
2021-03-20 02:33:36 +0530

    Navbar with Ganesha

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as Ganesha } from './assets/images/Lord-Ganesha-Vector.svg';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Nav">
+        <div className="Left"></div>
+        <div className="NavLeftPages"></div>
+        <div className="Ganesha">
+          <Ganesha
+            className="GaneshaIcon"
+            height="52px"
+            title="Jai Shree Ganesh"
+          />
+        </div>
+        <div className="NavRightPages"></div>
+        <div className="Right"></div>
+      </div>
+      <div className="Title">Title</div>
+      <div className="Main">Main</div>
+      <div className="Actions">Actions</div>
     </div>
   );
-}
+};
 
 export default App;
