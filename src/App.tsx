f2eb9e9df072204cb2bbebfd7d5fb76bb8d0aebd
Antriksh Singh <a0s00vf@homeoffice.wal-mart.com>
2021-03-21 23:25:48 +0530

    Add icon and interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useIdleTimer } from 'react-idle-timer';
+
 import './App.css';
 import Nav from 'components/Nav/Nav';
 import Jumbo from 'components/Jumbo/Jumbo';
@@ -7,6 +9,32 @@ import Main from 'components/Main/Main';
 
 const App: React.FC = () => {
   const [english, setEnglish] = useState<boolean>(true);
+  const [active, setActive] = useState<boolean>(true);
+
+  const handleOnIdle = () => {
+    setActive(false);
+    // console.log('user is idle', event);
+    console.log('last active', getLastActiveTime());
+  };
+
+  const handleOnActive = () => {
+    setActive(true);
+    // console.log('user is active', event);
+    console.log('time remaining', getRemainingTime());
+  };
+
+  // const handleOnAction = (e: any) => {
+  //   console.log('user did something', e);
+  // };
+
+  const { getRemainingTime, getLastActiveTime } = useIdleTimer({
+    timeout: 1000 * 5,
+    onIdle: handleOnIdle,
+    onActive: handleOnActive,
+    // onAction: handleOnAction,
+    // debounce: 500,
+  });
+
   return (
     <div className="App">
       <Nav />
@@ -14,11 +42,17 @@ const App: React.FC = () => {
       <Main isEnglish={english} />
 
       <div className="Actions">
-        <div className="Language">
-          <div className="Button" onClick={() => setEnglish(true)}>
+        <div className={`Language ${active ? '' : 'Hidden'}`}>
+          <div
+            className={`Button ${english ? '' : 'BtnNotActive'}`}
+            onClick={() => setEnglish(true)}
+          >
             English
           </div>
-          <div className="Button" onClick={() => setEnglish(false)}>
+          <div
+            className={`Button ${english ? 'BtnNotActive' : ''}`}
+            onClick={() => setEnglish(false)}
+          >
             हिन्दी
           </div>
         </div>
