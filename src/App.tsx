9150172a8717b0ddad96f45726ec3716df7edf64
Antriksh Singh <a0s00vf@homeoffice.wal-mart.com>
2021-03-28 06:16:21 +0530

    add audio

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Nav from 'components/Nav/Nav';
 import Jumbo from 'components/Jumbo/Jumbo';
 import Main from 'components/Main/Main';
 import ActionBar from 'components/ActionBar/ActionBar';
+import sound from 'assets/sounds/adaytoremember.mp3';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <audio src={sound} autoPlay loop muted />
       <LangProvider>
         <PageProvider>
           <Nav />
