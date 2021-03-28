2ae340eb3eee79c99b793afbd5936d95d6573e4b
Antriksh Singh <a0s00vf@homeoffice.wal-mart.com>
2021-03-28 05:34:11 +0530

    Add new banners

--- src/components/Jumbo/Jumbo.tsx
+++ src/components/Jumbo/Jumbo.tsx
@@ -6,8 +6,25 @@ import L3 from 'assets/images/L3-BackFlower.png';
 import L4 from 'assets/images/L4-US.png';
 import L5 from 'assets/images/L5-FrontFlower.png';
 import L6 from 'assets/images/L6-Banner-HA.png';
+import L60 from 'assets/images/L6-Banner-E.png';
+import L61 from 'assets/images/L6-Banner-F.png';
+import { usePage } from 'context/AppContext';
 
 const Jumbo: React.FC = () => {
+  const page = usePage();
+  console.log(page.state.index);
+  const banner = () => {
+    switch (page.state.index) {
+      case 0:
+        return <img className="Art" src={L60} alt="Layer 6" />;
+      case 1:
+        return <img className="Art" src={L6} alt="Layer 6" />;
+      case 2:
+        return <img className="Art" src={L61} alt="Layer 6" />;
+      default:
+        return null;
+    }
+  };
   return (
     <div className="Jumbo">
       <div className="BaseImg">
@@ -28,9 +45,7 @@ const Jumbo: React.FC = () => {
       <div className="Img">
         <img className="Art" src={L5} alt="Layer 5" />
       </div>
-      <div className="Img">
-        <img className="Art" src={L6} alt="Layer 6" />
-      </div>
+      <div className="Img">{banner()}</div>
     </div>
   );
 };
